7ca5ed4800c9eab7f28a5863af2347b01e70915a
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-04-23 12:49:32 -0400

    finished testing suite

--- api/server.test.js
+++ api/server.test.js
@@ -22,8 +22,31 @@ test('sanity', () => {
 
 describe('[POST] /api/auth/register', () => {
   it('responds with 201', async () => {
-    let res
-    res = await request(server).post('/api/auth/register').send(alice)
+    let res;
+    res = await request(server).post('/api/auth/register').send(alice);
     expect(res.status).toBe(201);
-  })
-})
+  });
+
+  it('responds with correct name on register', async () => {
+    const res = await request(server).post('/api/auth/register').send(alice);
+    expect(res.body.username).toBe(alice.username);
+  });
+
+  it('responds with an incorrect password match due to hash', async () => {
+    const res = await request(server).post('/api/auth/register').send(val);
+    expect(res.body.password).not.toBe(val.password);
+  });
+});
+
+describe('[POST] /api/auth/login', () => {
+  it('gives invalid credential message on bad credentials', async () => {
+    const logged = await request(server).post('/api/auth/login').send(alice);
+    expect(logged.body.message).toBe("invalid credentials");
+  });
+
+  it('brings back a welcome message', async () => {
+    await request(server).post('/api/auth/register').send(alice);
+    const logged = await request(server).post('/api/auth/login').send(alice);
+    expect(logged.body.message).toBe("welcome, Alice");
+  });
+});
